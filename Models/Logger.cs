167c806ad604f533f7ee35ce2c6ca93ea2794f7b
JakubMrozek <jakub.mrozek@gmail.com>
2013-05-17 20:27:12 +0200

    Pridano nastaveni settings.xml pro nastaveni logovani

--- Models/Logger.cs
+++ Models/Logger.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
+using System.Xml;
 
 namespace Blog.Models
 {
     public class Logger
     {
+
+        public bool IsEnable()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(GetXmlPath());
+                XmlNode node = doc.SelectSingleNode("nastaveni/logovani");
+                return node.InnerText == "zapnuto";
+
+            }
+            catch (FileNotFoundException e) 
+            {
+                return false;
+            }
+
+
+        }
+
         public void Log(string query)
         {
+            if (!IsEnable()) return;
+
             StreamWriter file = new StreamWriter(GetFilePath(), true);
             file.WriteLine(query);
             file.Close();
@@ -23,11 +45,16 @@ namespace Blog.Models
             return content;
         }
 
+        private string GetXmlPath()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            return string.Format("{0}/{1}", baseDir, "settings.xml");
+        }
+
         private string GetFilePath()
         {
             string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            string logFile = string.Format("{0}/Data/{1}", baseDir, "log.txt");
-            return logFile;
+            return string.Format("{0}/Data/{1}", baseDir, "log.txt");
         }
     }
 }
